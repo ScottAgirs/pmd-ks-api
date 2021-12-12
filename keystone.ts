7089397ed2035a6c04c8fea85ce483e6e7e934a6
ScottAgirs <scott@ijs.to>
2021-12-12 16:24:49 +0200

    INITIAL 💉
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ keystone.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+
+import { config } from '@keystone-6/core';
+
+import { lists } from './schema';
+
+import { withAuth, session } from './auth';
+
+const FRONTEND_URL:string = process.env.FRONTEND_URL as string;
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      provider: 'postgresql',
+      url: 'postgresql://doadmin:4dvbMUL0jSI1b3Z4@db-ks-dev-do-user-8624530-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require',
+    },
+    ui: {
+      isAccessAllowed: (context) => !!context.session?.data,
+    },
+    lists,
+    session,
+  })
+);
---
+++ schema.ts
@@ -0,0 +1,55 @@
+import { list } from '@keystone-6/core';
+
+import {
+  text,
+  relationship,
+  password,
+  timestamp,
+  select,
+} from '@keystone-6/core/fields';
+
+export const lists = {
+  User: list({
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      email: text({
+        validation: { isRequired: true },
+        isIndexed: 'unique',
+        isFilterable: true,
+      }),
+      password: password({ validation: { isRequired: true } }),
+      prompts: relationship({ ref: 'Prompt.user', many: true }),
+    },
+    ui: {
+      listView: {
+        initialColumns: ['name', 'prompts'],
+      },
+    },
+  }),
+  Prompts: list({
+    fields: {
+      title: text(),
+      status: select({
+        options: [
+          { label: 'Published', value: 'published' },
+          { label: 'Draft', value: 'draft' },
+        ],
+        defaultValue: 'draft',
+        ui: {
+          displayMode: 'segmented-control',
+        },
+      }),
+      publishDate: timestamp(),
+      author: relationship({
+        ref: 'User.prompts',
+        ui: {
+          displayMode: 'cards',
+          cardFields: ['name', 'email'],
+          inlineEdit: { fields: ['name', 'email'] },
+          linkToItem: true,
+          inlineCreate: { fields: ['name', 'email'] },
+        },
+      }),
+    },
+  }),
+};
