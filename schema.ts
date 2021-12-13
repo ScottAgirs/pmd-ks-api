779ced7300ab67c0e6d2c631b2ef5c5376f67ea7
ScottAgirs <scott@ijs.to>
2021-12-13 16:56:56 +0200

    feat(Schema): Add Booking Calendar Doctor DoctorLanguage
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- schema.ts
+++ schema.ts
@@ -1,9 +1,17 @@
+import { Booking } from "./api/booking/Booking";
 import { User } from "./api/user/User";
 import { Patient } from "./api/patient/Patient";
 import { Prompt } from "./api/prompt/Prompt";
+import { Calendar } from "./api/calendar/Calendar";
+import { Doctor } from "./api/doctor/Doctor";
+import { DoctorLanguage } from "./api/doctor-language/DoctorLanguage";
 
 
 export const lists = {
+  Booking,
+  Calendar,
+  Doctor,
+  DoctorLanguage,
   User,
   Patient,
   Prompt,
