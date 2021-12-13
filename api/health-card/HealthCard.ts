8dc7ca33087c6e06e628cca77d8f2556bfb187b4
ScottAgirs <scott@ijs.to>
2021-12-13 18:41:28 +0200

    feat(Lists): Init HealthCard
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/health-card/HealthCard.ts
@@ -0,0 +1,18 @@
+import { checkbox, integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const HealthCard = list({
+  fields: {
+    // TODO: patient
+    // patient: relationship({ ref: 'Patient.healthCards', many: false }),
+    isVerified: checkbox(),
+    expiryDate: timestamp({ validation: { isRequired: true } }),
+    nameOnCard: text({ validation: { isRequired: true } }),
+    insurancePolicyNumber: text({ validation: { isRequired: true } }),
+    dateOfBirth: timestamp({ validation: { isRequired: true } }),
+    // // TODO: Country
+    // country: relationship({ ref: 'Patient.visitedDoctors', many: true }),
+    versionCode: text({ validation: { isRequired: true } }),
+  },
+}) 
