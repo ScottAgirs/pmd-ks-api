917343ac8cc0c73bd36db1bd99ee48b8f0059e39
ScottAgirs <scott@ijs.to>
2021-12-14 17:36:02 +0200

    refactor(StepperStepProg): percentDone not required
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/stepper-step-prog/StepperStepProg.ts
+++ api/stepper-step-prog/StepperStepProg.ts
@@ -5,7 +5,7 @@ const { list } = require("@keystone-6/core");
 export const StepperStepProg = list({
   fields: {
     name: text(),
-    percentDone: integer({ validation: { isRequired: true } }),
+    percentDone: integer(),
     isCompleted: checkbox(),
     stepper: relationship({ ref: 'Stepper.stepperStepProgs', many: false }),
     stepperStep: relationship({ ref: 'StepperStep.stepperStepProg', many: false }),
