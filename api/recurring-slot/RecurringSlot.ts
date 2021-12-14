3d0458ae5aa3e78f23bbf7a57f8e9dddeba1e59b
ScottAgirs <scott@ijs.to>
2021-12-14 14:03:41 +0200

    feat(List): Init RecurringSlot
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/recurring-slot/RecurringSlot.ts
@@ -0,0 +1,14 @@
+import { checkbox, integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const RecurringSlot = list({
+  fields: {
+    wday: text(),
+    isActive: checkbox(),
+    interval: text(),
+    intervalCount: integer(),
+    schedule: relationship({ ref: 'Schedule.recurringSlots', many: false }),
+    timeIntervals: relationship({ ref: 'TimeInterval.recurringSlot', many: true }),
+  },
+})
