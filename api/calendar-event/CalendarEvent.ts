270b77c0ddccd213c2908120cc0c662e0d130c4f
ScottAgirs <scott@ijs.to>
2021-12-14 12:11:09 +0200

    feat(Lists): Init CalendarEvent
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/calendar-event/CalendarEvent.ts
@@ -0,0 +1,19 @@
+import { checkbox, integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const CalendarEvent = list({
+  fields: {
+    description: text({ validation: { isRequired: true } }),
+    title: text({ validation: { isRequired: true } }),
+    isConfirmationRequired: checkbox(),
+    // // TODO: calendar
+    // calendar: relationship({ ref: 'Calendar.calendarEvents', many: false }),
+    // // TODO: bookings
+    // bookings: relationship({ ref: 'Booking.calendarEvent', many: false }),
+    // // TODO: schedule
+    // patient: relationship({ ref: 'Patient.bookings', many: false }),
+    // // TODO: calendarEventType
+    // patient: relationship({ ref: 'Patient.bookings', many: false }),
+  },
+})
