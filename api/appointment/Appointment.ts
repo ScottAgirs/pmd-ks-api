6adb2181063a2f24ae479ec0a5628b2ce57475b6
ScottAgirs <scott@ijs.to>
2021-12-14 11:47:11 +0200

    feat(Lists): Init Appt
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/appointment/Appointment.ts
@@ -0,0 +1,14 @@
+import { relationship, timestamp } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const Appointment = list({
+  fields: {
+    // doctor: relationship({ ref: 'Doctor.appointments', many: false }),
+    // patient: relationship({ ref: 'Patient.appointments', many: false }),
+    scheduledStartsAt: timestamp(),
+    scheduledEndsAt: timestamp(),
+    startedAt: timestamp(),
+    endedAt: timestamp(),
+  },
+}) 
