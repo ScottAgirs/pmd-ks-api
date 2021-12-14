f1b843e7ed1dea0b6e21fa9960b4ebf1798c0b3d
ScottAgirs <scott@ijs.to>
2021-12-14 14:03:22 +0200

    feat(List): Init TimeInterval
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/time-interval/TimeInterval.ts
@@ -0,0 +1,11 @@
+import { relationship, text } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const TimeInterval = list({
+  fields: {
+    from: text(),
+    to: text(),
+    recurringSlot: relationship({ ref: 'RecurringSlot.timeIntervals', many: false }),
+  },
+})
