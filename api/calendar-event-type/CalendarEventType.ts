7aea5538d2297bb00a78723015966f601e391f50
ScottAgirs <scott@ijs.to>
2021-12-14 12:15:04 +0200

    feat(Lists): Init CalendarEventType
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/calendar-event-type/CalendarEventType.ts
@@ -0,0 +1,11 @@
+import { checkbox, integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const CalendarEventType = list({
+  fields: {
+    label: text({ validation: { isRequired: true } }),
+    value: text({ validation: { isRequired: true } }),
+    events: relationship({ ref: 'CalendarEvent.calendarEventType', many: true }),
+  },
+})
