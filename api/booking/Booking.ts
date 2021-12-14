5bec403d6b8cc791f7a14c13797f636fa1d7d5d4
ScottAgirs <scott@ijs.to>
2021-12-14 12:28:52 +0200

    refactor(Booking): Link to doc + patch rel type
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/booking/Booking.ts
+++ api/booking/Booking.ts
@@ -5,13 +5,13 @@ const { list } = require("@keystone-6/core");
 export const Booking = list({
   fields: {    
     // calendar: relationship({ ref: 'Calendar.bookings', many: false }),
-    // TODO: doctor
+    doctor: relationship({ ref: 'Doctor.bookings', many: false }),
     durationMins: integer({ validation: { isRequired: true } }),
-    event: relationship({ ref: 'CalendarEvent.bookings', many: true }),
+    event: relationship({ ref: 'CalendarEvent.bookings', many: false }),
     isConfirmed: checkbox(),
     name: text(),
     notes: text({ validation: { isRequired: false } }),
-    patient: relationship({ ref: 'Patient.bookings', many: true }),
+    patient: relationship({ ref: 'Patient.bookings', many: false }),
     startsAt: timestamp(),
   },
 })
