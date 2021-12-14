133d0860eb19c2ff58ba4228565192dc3b1dd627
ScottAgirs <scott@ijs.to>
2021-12-14 12:47:54 +0200

    feat(List): Init Schedule
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/schedule/Schedule.ts
@@ -0,0 +1,13 @@
+import { relationship, text } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const Schedule = list({
+  fields: {
+    title: text({ validation: { isRequired: true } }),
+    description: text(),
+    calendar: relationship({ ref: 'Calendar.schedules', many: false }),
+    events: relationship({ ref: 'CalendarEvent.schedule', many: true }),
+    defaultOn: relationship({ ref: 'Calendar.defaultSchedule', many: false }),
+  },
+}) 
