d9e0fcbd65fdb76547ebefc061d6cb68df5f71f9
ScottAgirs <scott@ijs.to>
2021-12-14 12:47:05 +0200

    feat(CalendarEvent): Add durationMins, schedule
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/calendar-event/CalendarEvent.ts
+++ api/calendar-event/CalendarEvent.ts
@@ -5,10 +5,12 @@ const { list } = require("@keystone-6/core");
 export const CalendarEvent = list({
   fields: {
     description: text({ validation: { isRequired: true } }),
+    durationMins: integer({ validation: { isRequired: true } }),
     title: text({ validation: { isRequired: true } }),
     isConfirmationRequired: checkbox(),
     calendar: relationship({ ref: 'Calendar.events', many: false }),
     bookings: relationship({ ref: 'Booking.event', many: true }),
     calendarEventType: relationship({ ref: 'CalendarEventType.events', many: false }),
+    schedule: relationship({ ref: 'Schedule.events', many: false }),
   },
 })
