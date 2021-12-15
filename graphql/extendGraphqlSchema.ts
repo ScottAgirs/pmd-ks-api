63973a00ce1b0ac646d2593bdbf522f514f4c422
ScottAgirs <scott@ijs.to>
2021-12-15 14:37:35 +0200

    feat(Schema): Add types, resolvers > createCalendarEventBooking
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- graphql/extendGraphqlSchema.ts
+++ graphql/extendGraphqlSchema.ts
@@ -1,6 +1,7 @@
 import { graphQLSchemaExtension } from "@keystone-6/core";
 
 import { completeMyStepProg } from "../api/stepper-step-prog/resolvers/completeMyStepProg";
+import { createCalendarEventBooking } from "../api/booking/resolvers/createCalendarEventBooking";
 
 import { typeDefs } from "./typeDefs";
 
@@ -9,6 +10,9 @@ export const extendGraphqlSchema = graphQLSchemaExtension({
   typeDefs,
   resolvers: {
     Mutation: {
+      // Bookings
+      createCalendarEventBooking,
+      // Stepper
       completeMyStepProg,
     }
   }
--- graphql/typeDefs.ts
+++ graphql/typeDefs.ts
@@ -2,6 +2,9 @@ const graphql = String.raw;
 
 export const typeDefs =  graphql`
   type Mutation {
+    # Bookings
+    createCalendarEventBooking(eventId: ID!, notes: String, startsAt: DateTime): StepperStepProg
+    # Stepper
     completeMyStepProg(stepId: ID!): StepperStepProg
   }
 `
