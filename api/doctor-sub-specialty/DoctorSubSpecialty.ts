383382ddcbd0bec8130e6e3911d8ae21456496cd
ScottAgirs <scott@ijs.to>
2021-12-16 19:03:51 +0200

    feat(Schema): Add DoctorSubSpecialty
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/doctor-sub-specialty/DoctorSubSpecialty.ts
@@ -0,0 +1,11 @@
+import { checkbox, integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const DoctorSubSpecialty = list({
+  fields: {
+    label: text({ validation: { isRequired: true } }),
+    value: text({ validation: { isRequired: true } }),
+    doctors: relationship({ ref: 'Doctor.doctorSubSpecialty', many: true }),
+  },
+})
