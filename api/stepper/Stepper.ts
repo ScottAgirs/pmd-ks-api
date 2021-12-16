3afa561237913390ecef88c42d5124ae0ddd7a14
ScottAgirs <scott@ijs.to>
2021-12-16 23:06:30 +0200

    chore(Stepper): Remove implicit def
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/stepper/Stepper.ts
+++ api/stepper/Stepper.ts
@@ -9,7 +9,7 @@ export const Stepper = list({
     slug: text(),
     description: text(),
     eyebrow: text(),
-    prompt: relationship({ ref: 'Prompt.stepper', many: false }),
+    prompt: relationship({ ref: 'Prompt.stepper' }),
     stepperSteps: relationship({ ref: 'StepperStep.stepper', many: true }),
     stepperProgs: relationship({ ref: 'StepperProg.stepper', many: true }),
     stepperStepProgs: relationship({ ref: 'StepperStepProg.stepper', many: true }),
