c371e64ff56fe098f0f6c1be03dd3d0263d7a525
ScottAgirs <scott@ijs.to>
2021-12-16 23:08:10 +0200

    chore(Appointment): Remove implicit def
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/appointment/Appointment.ts
+++ api/appointment/Appointment.ts
@@ -4,8 +4,8 @@ const { list } = require("@keystone-6/core");
 
 export const Appointment = list({
   fields: {
-    doctor: relationship({ ref: 'Doctor.appointments', many: false }),
-    patient: relationship({ ref: 'Patient.appointments', many: false }),
+    doctor: relationship({ ref: 'Doctor.appointments' }),
+    patient: relationship({ ref: 'Patient.appointments' }),
     scheduledStartsAt: timestamp(),
     scheduledEndsAt: timestamp(),
     startedAt: timestamp(),
