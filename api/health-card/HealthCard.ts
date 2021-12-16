2f27e8246a450fd76bfe3fdb3d8869823e2463f9
ScottAgirs <scott@ijs.to>
2021-12-16 23:07:23 +0200

    chore(HealthCard): Remove implicit def
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/health-card/HealthCard.ts
+++ api/health-card/HealthCard.ts
@@ -4,13 +4,13 @@ const { list } = require("@keystone-6/core");
 
 export const HealthCard = list({
   fields: {
-    country: relationship({ ref: 'Country.healthCards', many: false }),
+    country: relationship({ ref: 'Country.healthCards' }),
     dateOfBirth: timestamp({ validation: { isRequired: true } }),
     expiryDate: timestamp({ validation: { isRequired: true } }),
     insurancePolicyNumber: text({ validation: { isRequired: true } }),
     isVerified: checkbox(),
     nameOnCard: text({ validation: { isRequired: true } }),
-    patient: relationship({ ref: 'Patient.healthCards', many: false }),
+    patient: relationship({ ref: 'Patient.healthCards' }),
     versionCode: text({ validation: { isRequired: true } }),
   },
 }) 
