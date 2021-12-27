110d8ff1c7603d80212a664b8179422ffa049b94
ScottAgirs <scott@ijs.to>
2021-12-28 01:15:52 +0200

    feat(List): Init AppointmentVItal
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/appointment-vital/AppointmentVital.ts
@@ -0,0 +1,15 @@
+import { integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const AppointmentVital = list({
+  fields: {
+    appointment: relationship({ ref: 'Appointment.vitals' }),
+    patient: relationship({ ref: 'Patient.appointmentVitals', many: true }),
+    oxygenSaturation: integer(),
+    heartRate: integer(),
+    temperature: integer(),
+    bloodPressure: text(),
+    resp: integer(),
+  },
+}) 
