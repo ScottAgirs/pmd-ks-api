8802adc6ca2426698273671baa89fa3c6fd8f5a0
ScottAgirs <scott@ijs.to>
2021-12-28 01:15:46 +0200

    feat(List): Init AppointmentNote
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/appointment-note/AppointmentNote.ts
@@ -0,0 +1,12 @@
+import { integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const AppointmentNote = list({
+  fields: {
+    appointment: relationship({ ref: 'Appointment.note' }),
+    patient: relationship({ ref: 'Patient.appointmentNotes', many: true }),
+    body: text(),
+    title: text(),
+  },
+}) 
