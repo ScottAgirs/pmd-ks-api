0843454533056456dcec15e1ffae2f78508c4ceb
ScottAgirs <scott@ijs.to>
2021-12-28 01:18:54 +0200

    feat(Seed): Init dummy-doctors
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ seed/user/dummy-doctors.ts
@@ -0,0 +1,14 @@
+export const DUMMY_DOCTORS = [
+  {
+    doctorSince: "1999-07-05T00:00:00.000Z",
+  },
+  {
+    doctorSince: "2001-02-05T00:00:00.000Z",
+  },
+  {
+    doctorSince: "1978-07-05T00:00:00.000Z",
+  },
+  {
+    doctorSince: "2012-03-04T00:00:00.000Z",
+  },
+]; 
