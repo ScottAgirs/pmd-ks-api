ebeb8237d890d1fbd3acb816a672ea4a2c5f2dd2
ScottAgirs <scott@ijs.to>
2021-12-28 01:16:37 +0200

    feat(Appointment): Connect to booking, vitals, notes
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/appointment/Appointment.ts
+++ api/appointment/Appointment.ts
@@ -4,8 +4,11 @@ const { list } = require("@keystone-6/core");
 
 export const Appointment = list({
   fields: {
+    booking: relationship({ ref: 'Booking.appointment' }),
     doctor: relationship({ ref: 'Doctor.appointments' }),
+    note: relationship({ ref: 'AppointmentNote.appointment' }),
     patient: relationship({ ref: 'Patient.appointments' }),
+    vitals: relationship({ ref: 'AppointmentVital.appointment' }),
     scheduledStartsAt: timestamp(),
     scheduledEndsAt: timestamp(),
     startedAt: timestamp(),
