35a97c7d69ec18a7bf788be0fcb3e843af6b32b3
ScottAgirs <scott@ijs.to>
2021-12-28 22:34:00 +0200

    chore: Add TODO
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/appointment-vital/AppointmentVital.ts
+++ api/appointment-vital/AppointmentVital.ts
@@ -5,6 +5,9 @@ const { list } = require("@keystone-6/core");
 export const AppointmentVital = list({
   fields: {
     appointment: relationship({ ref: 'Appointment.vitalsData' }),
+    // TODO: See if performance would be affected 
+    // if appointmentVitals would be linked/accessible via patient.appointment.vitalsData instead,
+    // that way eliminating the need to replicate explicitly the connection to Patient here.
     patient: relationship({ ref: 'Patient.appointmentVitals', many: true }),
     feet: integer(),
     inches: integer(),
