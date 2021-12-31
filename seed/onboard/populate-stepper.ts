3110a33b260c9b4610d1e354f378bc67a7a5dccc
ScottAgirs <scott@ijs.to>
2021-12-31 19:49:06 +0200

    refactor(Seed): Populate sub-steppers
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- seed/onboard/populate-stepper.ts
+++ seed/onboard/populate-stepper.ts
@@ -9,35 +9,39 @@ interface StepperWithoutSteps {
 }
 
 export async function populateSteppers(keystone: any) {
-  console.log(`----------------------------------------`);
-  console.log(`🌱 Inserting Steppers Data: ${STEPPERS.length} items`);
-  console.log(`----------------------------------------`);
+  try {
+    console.log(`----------------------------------------`);
+    console.log(`🌱 Inserting Steppers Data: ${STEPPERS.length} items`);
+    console.log(`----------------------------------------`);
 
-  for (const stepper of STEPPERS) {
-    console.log(` 👨🏼‍⚕️ Adding [${stepper.name}] Specialty`);
-    const { stepperSteps } = stepper;
-    const stepperNoSteps:StepperWithoutSteps = stepper;
+    for (const stepper of STEPPERS) {
+      console.log(` 👨🏼‍⚕️ Adding [${stepper.name}] Specialty`);
+      const { stepperSteps } = stepper;
+      const stepperNoSteps:StepperWithoutSteps = stepper;
 
-    delete stepperNoSteps.stepperSteps;
+      delete stepperNoSteps.stepperSteps;
 
-    const createdStepper = await keystone.db.Stepper.createOne({
-      data: stepperNoSteps
-    });
-
-    console.log(` 👨🏼‍⚕️ ✅ Created [${createdStepper.name}]`);
-    console.log(` 🦶🏼 Adding ${stepperSteps.length} Steps to [${createdStepper.name}]`);
-    
-    for (const stepperStep of stepperSteps) {
-      const addedStepperStep = await keystone.db.StepperStep.createOne({
-        data: {
-          ...stepperStep,
-          stepper: { connect: { id: createdStepper.id } }
-        }
+      const createdStepper = await keystone.db.Stepper.createOne({
+        data: stepperNoSteps
       });
-      console.log(`🦶🏼 ✅ Added [${addedStepperStep.title}]`);
+
+      console.log(` 👨🏼‍⚕️ ✅ Created [${createdStepper.name}]`);
+      console.log(` 🦶🏼 Adding ${stepperSteps.length} Steps to [${createdStepper.name}]`);
+      
+      for (const stepperStep of stepperSteps) {
+        const addedStepperStep = await keystone.db.StepperStep.createOne({
+          data: {
+            ...stepperStep,
+            stepper: { connect: { id: createdStepper.id } }
+          }
+        });
+        console.log(`🦶🏼 ✅ Added [${addedStepperStep.title}]`);
+      }
     }
+    console.log(`✅ Steppers Seeded with ${STEPPERS.length} items`);
+    console.log(`👋 Please start the process with \`yarn dev\` or \`npm run dev\``);
+    // process.exit();
+  } catch (error) {
+    console.error('populateSteppers :: error', error);
   }
-  console.log(`✅ Steppers Seeded with ${STEPPERS.length} items`);
-  console.log(`👋 Please start the process with \`yarn dev\` or \`npm run dev\``);
-  process.exit();
 }
