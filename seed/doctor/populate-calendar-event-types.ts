e055921329350c5b6914795f30c79a60a3a834a9
ScottAgirs <scott@ijs.to>
2021-12-31 19:43:30 +0200

    refactor(Seed): Populate calendar event types
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- seed/doctor/populate-calendar-event-types.ts
+++ seed/doctor/populate-calendar-event-types.ts
@@ -1,18 +1,22 @@
 import { CALENDAR_EVENT_TYPES } from "./calendarEventTypes";
 
 export async function populateCalendarEventTypes(keystone: any) {
-  console.log(`----------------------------------------`);
-  console.log(`🌱 Inserting Calendar Event Types. Total ${CALENDAR_EVENT_TYPES.length} items`);
-  console.log(`----------------------------------------`);
+  try {
+    console.log(`----------------------------------------`);
+    console.log(`🌱 Inserting Calendar Event Types. Total ${CALENDAR_EVENT_TYPES.length} items`);
+    console.log(`----------------------------------------`);
 
-  for (const eventType of CALENDAR_EVENT_TYPES) {
-    console.log(` 🗓️ Adding [${eventType.label}] Event Type`);
-    const createdEventType = await keystone.db.CalendarEventType.createOne({
-      data: eventType
-    });
-    console.log(` 🗓️ ✅ Created [${createdEventType.label}]`);
+    for (const eventType of CALENDAR_EVENT_TYPES) {
+      console.log(` 🗓️ Adding [${eventType.label}] Event Type`);
+      const createdEventType = await keystone.db.CalendarEventType.createOne({
+        data: eventType
+      });
+      console.log(` 🗓️ ✅ Created [${createdEventType.label}]`);
+    }
+    console.log(`✅ Calendar Event Types Seeded with ${CALENDAR_EVENT_TYPES.length} items`);
+    console.log(`👋 Please start the process with \`yarn dev\` or \`npm run dev\``);
+    // process.exit();
+  } catch (error) {
+    console.error('populateCalendarEventTypes :: error', error); 
   }
-  console.log(`✅ Calendar Event Types Seeded with ${CALENDAR_EVENT_TYPES.length} items`);
-  console.log(`👋 Please start the process with \`yarn dev\` or \`npm run dev\``);
-  process.exit();
 }
