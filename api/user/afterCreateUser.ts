ce77c011dcb6acf75ce12715a62e859421515da3
ScottAgirs <scott@ijs.to>
2021-12-31 19:48:21 +0200

    refactor(User): mv calendar creation to afterCreateDoc
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/user/afterCreateUser.ts
+++ api/user/afterCreateUser.ts
@@ -10,30 +10,7 @@ export const afterCreateUser = async ({ context, item }) => {
       }
     },
   }})
-
-  const createdCalendar = await context.query.Calendar.createOne({ data: {
-    name: `${item.firstName}'s Calendar`,
-    user: {
-      connect: {
-        id: item.id
-      }
-    },
-  }})
-
-  await context.query.Schedule.createOne({ data: {
-    title: `${item.firstName}'s Default Schedule`,
-    calendar: {
-      connect: {
-        id: createdCalendar.id
-      }
-    },
-    defaultOn: {
-      connect: {
-        id: createdCalendar.id
-      }
-    }
-  }})
-
+  
   await context.query.Prompt.createOne({ data: {
     name: `${item.firstName}'s Patient Onboarding Prompt`,
     promptType: "redirect",
