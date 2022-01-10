b615ce9aed604b11537d489dfaecb7223edb945b
ScottAgirs <scott@ijs.to>
2022-01-11 01:25:53 +0200

    refactor(typeDefs): notes > reason
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- graphql/typeDefs.ts
+++ graphql/typeDefs.ts
@@ -3,7 +3,7 @@ const graphql = String.raw;
 export const typeDefs =  graphql`
   type Mutation {
     # Bookings
-    createCalendarEventBooking(eventId: ID!, notes: String, startsAt: DateTime): StepperStepProg
+    createCalendarEventBooking(eventId: ID!, reason: String, startsAt: DateTime): StepperStepProg
     # Stepper
     completeMyStepProg(stepId: ID!): StepperStepProg
   }
