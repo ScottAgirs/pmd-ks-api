100e5e0b4a6581be246e14f952e7843fe15ff3aa
ScottAgirs <scott@ijs.to>
2022-01-11 01:25:07 +0200

    feat(ApptNote): Add createdAt.timestam[p()
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/appointment-note/AppointmentNote.ts
+++ api/appointment-note/AppointmentNote.ts
@@ -5,6 +5,11 @@ const { list } = require("@keystone-6/core");
 export const AppointmentNote = list({
   fields: {
     appointment: relationship({ ref: 'Appointment.notes' }),
+    createdAt: timestamp({
+      defaultValue: {
+        kind: "now"
+      }
+    }),
     patient: relationship({ ref: 'Patient.appointmentNotes' }),
     summary: text(),
     title: text(),
