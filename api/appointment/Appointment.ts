afd1a58f350be884d0a80a8c2b622cae53645b1e
ScottAgirs <scott@ijs.to>
2022-01-21 12:27:07 +0100

    feat(Appt): Link prescriptions
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/appointment/Appointment.ts
+++ api/appointment/Appointment.ts
@@ -1,4 +1,5 @@
-import { relationship, timestamp } from "@keystone-6/core/fields";
+import { relationship, text, timestamp } from "@keystone-6/core/fields";
+import { document } from "@keystone-6/fields-document";
 
 const { list } = require("@keystone-6/core");
 
@@ -8,10 +9,25 @@ export const Appointment = list({
     doctor: relationship({ ref: 'Doctor.appointments' }),
     notes: relationship({ ref: 'AppointmentNote.appointment', many: true }),
     patient: relationship({ ref: 'Patient.appointments' }),
+    prescription: relationship({ ref: 'Prescription.appointment' }),
+    reason: text(),
     vitalsData: relationship({ ref: 'AppointmentVital.appointment' }),
     scheduledStartsAt: timestamp(),
     scheduledEndsAt: timestamp(),
     startedAt: timestamp(),
+    summaryString: text(),
+    summary: document({
+      formatting: true,
+      layouts: [
+        [1, 1],
+        [1, 1, 1],
+        [2, 1],
+        [1, 2],
+        [1, 2, 1],
+      ],
+      links: true,
+      dividers: true,
+    }),
     endedAt: timestamp(),
   },
 }) 
