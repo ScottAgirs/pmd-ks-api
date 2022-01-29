7daa9031bba9b3f62333d25b0fb152cd9281798e
ScottAgirs <scott@ijs.to>
2022-01-29 15:05:11 +0200

    chore: nts - rm clg
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/stepper-step-prog/services/createOrUpdateStepperStepProg.ts
+++ api/stepper-step-prog/services/createOrUpdateStepperStepProg.ts
@@ -93,7 +93,6 @@ export const createOrUpdateStepperStepProg = async (
     })
 
   } else {
-    console.log("Updating stepperStepProg");
     stepperStepProg = await context.db.StepperStepProg.updateOne({
       where: {
         id: stepperStepProg?.id as string,
