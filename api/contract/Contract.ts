e24f2cf4e9f5df0ebd891f4f9d15910a54263e4c
ScottAgirs <scott@ijs.to>
2022-01-29 15:04:41 +0200

    feat(List): Init Contract
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/contract/Contract.ts
@@ -0,0 +1,27 @@
+import { checkbox, relationship, text, timestamp } from "@keystone-6/core/fields";
+import { document } from "@keystone-6/fields-document";
+
+const { list } = require("@keystone-6/core");
+
+export const Contract = list({
+  fields: {
+    signedBy: relationship({ ref: 'User.contracts', many: true }),
+    isActive: checkbox(),
+    body: document({
+      formatting: true,
+      layouts: [
+        [1, 1],
+        [1, 1, 1],
+        [2, 1],
+        [1, 2],
+        [1, 2, 1],
+      ],
+      links: true,
+      dividers: true,
+    }),
+    name: text(),
+    slug: text(),
+    effectiveFrom: timestamp(),
+    lastUpdated: timestamp(),
+  },
+}) 
