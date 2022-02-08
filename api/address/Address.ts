38b7fe453bc3618427fcc8980436b53db8f940e4
ScottAgirs <scott@ijs.to>
2022-02-09 00:00:02 +0200

    feat(Lists): Init Address
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/address/Address.ts
@@ -0,0 +1,16 @@
+import { text } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const Address = list({
+  fields: {
+    country: text(),
+    addressLine1: text(),
+    addressLine2: text(),
+    administrativeArea: text(),
+    locality: text(),
+    postalCode: text(),
+    thoroughfare: text(),
+    premise: text(),
+  },
+}) 
