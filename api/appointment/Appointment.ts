2ea6dd3a4125f954bde5d1eccf46e65e47079467
ScottAgirs <scott@ijs.to>
2022-02-10 15:01:13 +0200

    feat(Appt): Add isSigned*
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/appointment/Appointment.ts
+++ api/appointment/Appointment.ts
@@ -1,12 +1,19 @@
-import { relationship, text, timestamp } from "@keystone-6/core/fields";
+import { checkbox, relationship, text, timestamp } from "@keystone-6/core/fields";
 import { document } from "@keystone-6/fields-document";
 
 const { list } = require("@keystone-6/core");
 
 export const Appointment = list({
   fields: {
+    billing: relationship({ ref: 'AppointmentBilling.appointment' }),
     booking: relationship({ ref: 'Booking.appointment' }),
     doctor: relationship({ ref: 'Doctor.appointments' }),
+    isSigned: checkbox(),
+    isSignedOn: timestamp({
+      defaultValue: {
+        kind: "now",
+      }
+    }),
     notes: relationship({ ref: 'AppointmentNote.appointment', many: true }),
     patient: relationship({ ref: 'Patient.appointments' }),
     prescription: relationship({ ref: 'Prescription.appointment' }),
