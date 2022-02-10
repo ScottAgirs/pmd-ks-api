c4430029a70d133bbef26c3eb4fb6a3a878865e5
ScottAgirs <scott@ijs.to>
2022-02-10 15:01:37 +0200

    feat(List): Init AppointmentBilling
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/appointment-billing/AppointmentBilling.ts
@@ -0,0 +1,12 @@
+import { relationship, text, timestamp } from "@keystone-6/core/fields";
+
+const { list } = require("@keystone-6/core");
+
+export const AppointmentBilling = list({
+  fields: {
+    appointment: relationship({ ref: 'Appointment.billing' }),
+    doctor: relationship({ ref: 'Doctor.billings' }),
+    // clinic: relationship({ ref: 'Clinic.billing' }),
+    billingItems: relationship({ ref: 'AppointmentBillingItem.billing', many: true }),
+  },
+}) 
