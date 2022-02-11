f495471d72e5e9e27881f6ea254fb656f002bf7a
ScottAgirs <scott@ijs.to>
2022-02-11 02:06:02 +0200

    feat(Billing): Add `status`
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/appointment-billing/AppointmentBilling.ts
+++ api/appointment-billing/AppointmentBilling.ts
@@ -1,4 +1,4 @@
-import { relationship, text, timestamp } from "@keystone-6/core/fields";
+import { checkbox, relationship, text, timestamp } from "@keystone-6/core/fields";
 
 const { list } = require("@keystone-6/core");
 
@@ -8,5 +8,6 @@ export const AppointmentBilling = list({
     doctor: relationship({ ref: 'Doctor.billings' }),
     // clinic: relationship({ ref: 'Clinic.billing' }),
     billingItems: relationship({ ref: 'AppointmentBillingItem.billing', many: true }),
+    status: text(),
   },
 }) 
