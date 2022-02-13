72e4a36af63321da57e7b93ed31e8c09ce94147e
ScottAgirs <scott@ijs.to>
2022-02-13 10:53:19 +0100

    feat(Schedule): Add timezone()
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/schedule/Schedule.ts
+++ api/schedule/Schedule.ts
@@ -4,12 +4,13 @@ const { list } = require("@keystone-6/core");
 
 export const Schedule = list({
   fields: {
-    title: text({ validation: { isRequired: true } }),
-    description: text(),
-    calendar: relationship({ ref: 'Calendar.schedules' }),
     // TODO: Link to bookings
     // bookings: relationship({ ref: 'Booking.schedule', many: true }),
+    calendar: relationship({ ref: 'Calendar.schedules' }),
+    description: text(),
     defaultOn: relationship({ ref: 'Calendar.defaultSchedule' }),
     recurringSlots: relationship({ ref: 'RecurringSlot.schedule', many: true }),
+    timezone: text(),
+    title: text({ validation: { isRequired: true } }),
   },
 }) 
