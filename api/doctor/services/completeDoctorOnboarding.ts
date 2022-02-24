cc8c2b189a369f4fb49e8f2e3998292a970d8004
ScottAgirs <scott@ijs.to>
2022-02-24 23:14:28 +0200

    feat(Doctor): Init completeOnb service
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/doctor/services/completeDoctorOnboarding.ts
@@ -0,0 +1,68 @@
+import { BaseItem, KeystoneContext } from "@keystone-6/core/types";
+
+const requiredDoctorFields = [
+  // @TODO: [ONBOARDING] Add required address field
+  // 'address',
+  'user',
+  // TODO: [IMPORTANT] Add rest of fields
+];
+
+export const completeDoctorOnboarding = async (
+    context: KeystoneContext,
+    stepperStepProg: any,
+  ) => {
+  const doctorUser = stepperStepProg.user;
+
+  if (stepperStepProg?.stepper?.slug === 'doctor') {
+    const userId = doctorUser.id;
+
+    const doctorDB = context.db.Doctor;
+    const userMatchedDoctors = await doctorDB.findMany({ 
+      where: {
+        user: {
+          id: { equals: userId },
+        }
+      }
+    });
+
+    const doctor = userMatchedDoctors.length > 0 ? userMatchedDoctors[0] : null;
+
+    if (!doctor) {
+      throw new Error('Doctor not found');
+    }
+    
+    const doctorId = doctor.id;
+
+    let isCompleteProfile = true;
+
+    // check if doctor has all the required fields
+    requiredDoctorFields.forEach(field => {
+      if (doctor[field] === null) {
+        console.warn('doctor[field]', `${field} is missing.`);
+        isCompleteProfile = false;
+      }
+    });
+
+    if (isCompleteProfile) {
+      const completedDoctor = await doctorDB.updateOne({
+        where: {
+          id: doctorId as string,
+        },
+        data: {
+          isCompleteProfile: true,
+        },
+      });
+      
+
+      // TODO: send email to doctor user
+
+      // @TODO: Add logging
+      console.log(
+        `Doctor's profile for user ${doctorUser.username} [ID: ${doctorUser.id}] is complete`
+      );
+
+      return completedDoctor;
+    }
+  }
+  return false;
+}
