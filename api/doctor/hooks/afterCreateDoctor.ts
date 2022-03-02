397263203ce46de75e2910e8ec6f5c5c8a5b2485
ScottAgirs <scott@ijs.to>
2022-03-02 18:50:33 +0200

    refactor(Doctor): hooks.afterCreate add safety checks
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/doctor/hooks/afterCreateDoctor.ts
+++ api/doctor/hooks/afterCreateDoctor.ts
@@ -10,67 +10,75 @@ export const afterCreateDoctor = async ({ context, item: doctor }: AfterCreateDo
   console.log('afterCreateDoctor :: doctor', doctor);
 
   const userDB = context.db.User;
-  const user = await userDB.findOne({ where: { id: doctor.userId as string } });
 
-  const createdCalendar = await context.db.Calendar.createOne({ data: {
-    name: `${user.firstName}'s Calendar`,
-    user: {
-      connect: {
-        id: user.id
-      }
-    },
-  }})
-
-  await context.db.Schedule.createOne({ data: {
-    title: `${user.firstName}'s Work Hours`,
-    tz: "America/Toronto",
-    calendar: {
-      connect: {
-        id: createdCalendar.id
-      }
-    },
-    defaultOn: {
-      connect: {
-        id: createdCalendar.id
-      }
-    }
-  }})
+  // This account for when we're Seeding as doctor.userId in afterCreate for some reason doesn't appear right away.
+  if (!doctor.userId) return null;
   
-  await context.db.Address.createOne({ data: {
-    doctorClinic: {
-      connect: {
-        id: doctor.id
+  try {
+    const user = await userDB.findOne({ where: { id: doctor.userId as string } });
+
+    const createdCalendar = await context.db.Calendar.createOne({ data: {
+      name: `${user.firstName}'s Calendar`,
+      user: {
+        connect: {
+          id: user.id
+        }
+      },
+    }})
+
+    await context.db.Schedule.createOne({ data: {
+      title: `${user.firstName}'s Work Hours`,
+      tz: "America/Toronto",
+      calendar: {
+        connect: {
+          id: createdCalendar.id
+        }
+      },
+      defaultOn: {
+        connect: {
+          id: createdCalendar.id
+        }
       }
+    }})
+    
+    await context.db.Address.createOne({ data: {
+      doctorClinic: {
+        connect: {
+          id: doctor.id
+        }
+      }
+    }})
+
+    const eventTypes = await context.db.CalendarEventType.findMany()
+
+    const sharedCreateCalendarEventParams = {
+      calendar: {
+        connect: {
+          id: createdCalendar.id
+        }
+      },
+      doctor: {
+        connect: {
+          id: doctor.id
+        }
+      },
+      durationMins: 15,
+      isConfirmationRequired: false,
     }
-  }})
 
-  const eventTypes = await context.db.CalendarEventType.findMany()
+    const createdCalendarEventsData = eventTypes.map(eventType => ({
+      ...sharedCreateCalendarEventParams,
+      eventType: {
+        connect: {
+          id: eventType.id
+        }
+      },
+      title: eventType.label,
+      description: `${eventType.label} appointment with Dr. ${user.firstName} ${user.firstName}`,
+    }))
 
-  const sharedCreateCalendarEventParams = {
-    calendar: {
-      connect: {
-        id: createdCalendar.id
-      }
-    },
-    doctor: {
-      connect: {
-        id: doctor.id
-      }
-    },
-    durationMins: 15,
-    isConfirmationRequired: false,
+    await context.db.CalendarEvent.createMany({ data: createdCalendarEventsData });
+  } catch (error) {
+    console.log('afterCreateDoctor :: error', doctor, error);
   }
-
-  const createdCalendarEventsData = eventTypes.map(eventType => ({
-    ...sharedCreateCalendarEventParams,
-    eventType: {
-      connect: {
-        id: eventType.id
-      }
-    },
-    title: eventType.label,
-    description: `${eventType.label} appointment with Dr. ${user.firstName} ${user.firstName}`,
-  }))
-
-  await context.db.CalendarEvent.createMany({ data: createdCalendarEventsData });
 }
