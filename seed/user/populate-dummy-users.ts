41c0e0f861afcd4b0f83b71ceab48ee97166ed83
ScottAgirs <scott@ijs.to>
2022-03-02 20:50:00 +0200

    refactor(Seed): Improve User.Doctor factory
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- seed/user/populate-dummy-users.ts
+++ seed/user/populate-dummy-users.ts
@@ -11,19 +11,32 @@ export async function populateDummyUsers(keystone: any) {
   for (const userSeedData of dummyUsers) {
     // https://rawgit.com/Marak/faker.js/master/examples/browser/index.html#helpers
 
-    const { doctor: doctorData, patient: patientData, ...user } = userSeedData;
+    const { doctor: doctorData = {}, patient: patientData, ...user } = userSeedData;
     
-    
+
     const { emergencyContacts, healthCards, ...patient } = patientData;
     
-    const normalizedDoctorData = doctorData ? { 
-      doctor: { create: doctorData, } 
-    } : {} 
+    // @ts-ignore
+    const { doctorSpecialty, doctorSubSpecialties, ...doctor } = doctorData;
+    const hasDoctor = !!Object.keys(doctor).length;
+
+    const createDoctorData = !hasDoctor ? {} : {
+      create: {
+        ...doctor,
+        doctorSpecialty: {
+          connect: { value: doctorSpecialty.value },
+        },
+        doctorSubSpecialties: {
+          connect: doctorSubSpecialties.map((sp:any) => ({ value: sp.value })),
+        }
+      } 
+    }
 
     await keystone.db.User.createOne({
       data: {
         ...user,
-        ...normalizedDoctorData,
+        // @ts-ignore
+        doctor: hasDoctor ? createDoctorData : undefined,
         patient: {
           create: {
             ...patient,
