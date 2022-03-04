1ba04c76d73fbefc3a66762d713938933b344118
ScottAgirs <scott@ijs.to>
2022-03-04 23:14:28 +0200

    feat(Session): Add supporting parts
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- keystone.ts
+++ keystone.ts
@@ -38,7 +38,7 @@ if (!sessionSecret) {
 const auth = createAuth({
   listKey: 'User',
   identityField: 'subjectId',
-  sessionData: `id username email firstName lastName photoSrc`,
+  sessionData: `id doctor { id } username email firstName lastName photoSrc patient { id }`,
   autoCreate: true,
   resolver: async (props: any) => {
     const username = slugify(props.user.name as string, {
@@ -125,7 +125,9 @@ export default auth.withAuth(
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
     ui: {
       // For our starter, we check that someone has session data before letting them see the Admin UI.
-      isAccessAllowed: (context) => !!context.session?.data,
+      isAccessAllowed: (context) => {
+        return !!context.session?.data
+      },
     },
     lists,
     session,
