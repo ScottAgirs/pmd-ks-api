604f86a772d5ac14b4c12485f2a505217b4b9013
ScottAgirs <scott@ijs.to>
2022-03-19 14:43:31 +0200

    patch(Seed): Populate stepper guard clauses
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- seed/onboard/populate-stepper.ts
+++ seed/onboard/populate-stepper.ts
@@ -15,53 +15,63 @@ export async function populateSteppers(keystone: any) {
     console.log(`----------------------------------------`);
 
     for (const stepper of STEPPERS) {
-      console.log(` 👨🏼‍⚕️ Adding [${stepper.name}] Stepper`);
+      console.log(` 👨🏼‍⚕️ Adding [${stepper?.name || "Unknown stepper"}] Stepper`);
       const { stepperSteps } = stepper;
       const stepperNoSteps: StepperWithoutSteps = stepper;
 
+      let createdStepper;
       const existing = await keystone.db.Stepper.findOne({
         where: { slug: stepper.slug },
       });
       if (existing) {
-        console.log(` 💀 [${stepper.name}] stepper already exists - skipping.`);
+        console.log(
+          ` 💀 [${
+            stepper?.name || "Unknown stepper"
+          }] stepper already exists - skipping.`
+        );
       } else {
         delete stepperNoSteps.stepperSteps;
 
-        let createdStepper;
         try {
           createdStepper = await keystone.db.Stepper.createOne({
             data: stepperNoSteps,
           });
         } catch (error) {
           console.error(error);
-          throw new Error(`Error creating stepper [${stepper.name}]`);
-        }
-
-        for (const stepperStep of stepperSteps) {
-          console.log(
-            ` 🦶🏼 Adding ${stepperSteps.length} Steps to [${createdStepper.name}]`
+          throw new Error(
+            `Error creating stepper [${stepper?.name || "Unknown stepper"}]`
           );
+        }
+      }
 
-          const existingStep = await keystone.db.StepperStep.findOne({
-            where: { slug: stepperStep.slug },
-          });
-          if (existingStep) {
-            console.log(
-              ` 💀 [${stepperStep.title}] stepper step already exists - skipping.`
+      for (const stepperStep of stepperSteps) {
+        console.log(
+          ` 🦶🏼 Adding ${stepperSteps.length} Step Steps to: ${
+            createdStepper?.name
+              ? `Created stepper: ${createdStepper.name}`
+              : `Seed Stepper ${stepper.name}`
+          }`
+        );
+
+        const existingStep = await keystone.db.StepperStep.findOne({
+          where: { slug: stepperStep.slug },
+        });
+        if (existingStep) {
+          console.log(
+            ` 💀 [${stepperStep.title}] stepper step already exists - skipping.`
+          );
+        } else {
+          try {
+            await keystone.db.StepperStep.createOne({
+              data: {
+                ...stepperStep,
+                stepper: { connect: { slug: stepper.slug } },
+              },
+            });
+          } catch (error) {
+            throw new Error(
+              `Error creating stepper step [${stepperStep.title}]`
             );
-          } else {
-            try {
-              await keystone.db.StepperStep.createOne({
-                data: {
-                  ...stepperStep,
-                  stepper: { connect: { id: createdStepper.id } },
-                },
-              });
-            } catch (error) {
-              throw new Error(
-                `Error creating stepper step [${stepperStep.title}]`
-              );
-            }
           }
         }
       }
