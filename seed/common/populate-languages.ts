66243f411df3a2d3c85b79f56142730add6b68d7
ScottAgirs <scott@ijs.to>
2022-03-19 14:23:50 +0200

    refactor(Seed): Add existing check to Populate Langs
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- seed/common/populate-languages.ts
+++ seed/common/populate-languages.ts
@@ -3,23 +3,31 @@ import { SUPPORTED_LANGUAGES } from "./languages";
 export async function populateLanguages(keystone: any) {
   try {
     console.log(`----------------------------------------`);
-    console.log(`🌱 Seeding [${SUPPORTED_LANGUAGES.length}] Languages`);
+    console.log(`🌱 Seeding [${SUPPORTED_LANGUAGES.length}] Languages 🌍`);
     console.log(`----------------------------------------`);
 
     for (const language of SUPPORTED_LANGUAGES) {
       console.log(` 👨🏼‍⚕️ Adding [${language.label}] Language`);
-
-      try {
-        await keystone.db.Language.createOne({
-          data: language
-        });
-      } catch (error) {
-        throw new Error(`Error creating language [${language.label}]`);
+      const existing = await keystone.db.Language.findOne({
+        where: { value: language.value },
+      });
+      if (existing) {
+        console.log(
+          ` 🌍 Language [${language.label}] already exists - skipping.`
+        );
+      } else {
+        try {
+          await keystone.db.Language.createOne({
+            data: language,
+          });
+        } catch (error) {
+          throw new Error(`Error creating language [${language.label}]`);
+        }
       }
     }
 
     console.log(`✅ Seeded [${SUPPORTED_LANGUAGES.length}] Languages 🌳`);
   } catch (error) {
-    console.error('populateLanguages :: error', error);
+    console.error("populateLanguages :: error", error);
   }
 }
