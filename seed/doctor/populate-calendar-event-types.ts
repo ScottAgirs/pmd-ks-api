3d8b3bae6f29a627c918f15b1d67b11e4055dfd2
ScottAgirs <scott@ijs.to>
2022-03-19 14:24:00 +0200

    refactor(Seed): Add existing check to Populate CalEvs
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- seed/doctor/populate-calendar-event-types.ts
+++ seed/doctor/populate-calendar-event-types.ts
@@ -3,21 +3,35 @@ import { CALENDAR_EVENT_TYPES } from "./calendarEventTypes";
 export async function populateCalendarEventTypes(keystone: any) {
   try {
     console.log(`----------------------------------------`);
-    console.log(`🌱 Seeding [${CALENDAR_EVENT_TYPES.length}] Calendar Event Types.`);
+    console.log(
+      `🌱 Seeding [${CALENDAR_EVENT_TYPES.length}] Calendar Event Types.`
+    );
     console.log(`----------------------------------------`);
 
     for (const eventType of CALENDAR_EVENT_TYPES) {
       console.log(` 🗓️ Adding [${eventType.label}] Event Type`);
-      try {
-        await keystone.db.CalendarEventType.createOne({
-          data: eventType
-        });
-      } catch (error) {
-        throw new Error(`Error creating event type [${eventType.label}]`); 
+
+      const existing = await keystone.db.CalendarEventType.findOne({
+        where: { value: eventType.value },
+      });
+      if (existing) {
+        console.log(
+          ` 🗓️ Event Type [${eventType.label}] already exists - skipping.`
+        );
+      } else {
+        try {
+          await keystone.db.CalendarEventType.createOne({
+            data: eventType,
+          });
+        } catch (error) {
+          throw new Error(`Error creating event type [${eventType.label}]`);
+        }
       }
     }
-    console.log(`✅ Seeded [${CALENDAR_EVENT_TYPES.length}] Calendar Event Types 🌳`);
+    console.log(
+      `✅ Seeded [${CALENDAR_EVENT_TYPES.length}] Calendar Event Types 🌳`
+    );
   } catch (error) {
-    console.error('populateCalendarEventTypes :: error', error); 
+    console.error("populateCalendarEventTypes :: error", error);
   }
 }
