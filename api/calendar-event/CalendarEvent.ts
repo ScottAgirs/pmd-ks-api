54370a15c50656116baf081e1b0787448dc17b21
ScottAgirs <scott@ijs.to>
2022-03-19 14:22:12 +0200

    refactor(CalEv):  sort
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/calendar-event/CalendarEvent.ts
+++ api/calendar-event/CalendarEvent.ts
@@ -1,17 +1,23 @@
-import { checkbox, integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+import {
+  checkbox,
+  integer,
+  relationship,
+  text,
+  timestamp,
+} from "@keystone-6/core/fields";
 
 const { list } = require("@keystone-6/core");
 
 export const CalendarEvent = list({
   fields: {
-    bookings: relationship({ ref: 'Booking.event', many: true }),
-    calendar: relationship({ ref: 'Calendar.events' }),
     description: text(),
-    doctor: relationship({ ref: 'Doctor.calendarEvents' }),
     durationMins: integer({ validation: { isRequired: true } }),
-    eventType: relationship({ ref: 'CalendarEventType.events' }),
     isActive: checkbox(),
     isConfirmationRequired: checkbox(),
     title: text({ validation: { isRequired: true } }),
+    bookings: relationship({ ref: "Booking.event", many: true }),
+    calendar: relationship({ ref: "Calendar.events" }),
+    eventType: relationship({ ref: "CalendarEventType.events" }),
+    doctor: relationship({ ref: "Doctor.calendarEvents" }),
   },
-})
+});
