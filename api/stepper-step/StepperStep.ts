eaa3239cd24b38eef4ac3e88fd0d25bd662280b2
ScottAgirs <scott@ijs.to>
2022-03-19 14:43:05 +0200

    refactor(Stepper): Make slug indexed
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/stepper-step/StepperStep.ts
+++ api/stepper-step/StepperStep.ts
@@ -1,4 +1,4 @@
-import { checkbox, integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+import { checkbox, integer, relationship, text } from "@keystone-6/core/fields";
 
 const { list } = require("@keystone-6/core");
 
@@ -9,9 +9,11 @@ export const StepperStep = list({
     isFinal: checkbox(),
     label: text(),
     order: integer({ validation: { isRequired: true } }),
-    slug: text(),
-    stepper: relationship({ ref: 'Stepper.stepperSteps' }),
-    stepperStepProg: relationship({ ref: 'StepperStepProg.stepperStep' }),
+    slug: text({
+      isIndexed: "unique",
+    }),
+    stepper: relationship({ ref: "Stepper.stepperSteps" }),
+    stepperStepProg: relationship({ ref: "StepperStepProg.stepperStep" }),
     title: text(),
   },
-})
+});
