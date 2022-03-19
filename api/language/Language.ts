c804ca5b57bb9446418f2c388169d4cd8db4768a
ScottAgirs <scott@ijs.to>
2022-03-19 14:22:28 +0200

    refactor(Contract):  sort + make value indexed
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/language/Language.ts
+++ api/language/Language.ts
@@ -1,11 +1,17 @@
-import { checkbox, integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+import {
+  checkbox,
+  integer,
+  relationship,
+  text,
+  timestamp,
+} from "@keystone-6/core/fields";
 
 const { list } = require("@keystone-6/core");
 
 export const Language = list({
   fields: {
     label: text({ validation: { isRequired: false } }),
-    value: text({ validation: { isRequired: false } }),
-    doctors: relationship({ ref: 'Doctor.languages', many: true }),
+    value: text({ validation: { isRequired: false }, isIndexed: "unique" }),
+    doctors: relationship({ ref: "Doctor.languages", many: true }),
   },
-}) 
+});
