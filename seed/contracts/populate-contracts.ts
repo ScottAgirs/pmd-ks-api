f0bbb449aaae8ee3ac6a75bda874524625b4f22a
ScottAgirs <scott@ijs.to>
2022-03-19 14:23:55 +0200

    refactor(Seed): Add existing check to Populate Contracts
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- seed/contracts/populate-contracts.ts
+++ seed/contracts/populate-contracts.ts
@@ -9,21 +9,28 @@ export async function populateContracts(keystone: any) {
     for (const contract of DOCTOR_CONTRACTS) {
       console.log(` 👨🏼‍⚕️ Adding [${contract.name}] Contract`);
 
-
-
-      let createdContract;
-      try {
-        createdContract = await keystone.db.Contract.createOne({
-          data: contract
-        });
-      } catch (error) {
-        console.error(error);
-        throw new Error(`Error creating contract [${contract.name}]`);
+      const existing = await keystone.db.Contract.findOne({
+        where: { slug: contract.slug },
+      });
+      if (existing) {
+        console.log(
+          ` 💾 Contract [${contract.slug}] already exists - skipping.`
+        );
+      } else {
+        let createdContract;
+        try {
+          createdContract = await keystone.db.Contract.createOne({
+            data: contract,
+          });
+        } catch (error) {
+          console.error(error);
+          throw new Error(`Error creating contract [${contract.name}]`);
+        }
       }
     }
 
     console.log(`✅ Seeded [${DOCTOR_CONTRACTS.length}] Contracts 🌳`);
   } catch (error) {
-    console.error('populateContracts :: error', error);
+    console.error("populateContracts :: error", error);
   }
 }
