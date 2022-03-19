40fee011cb4ad1355e8b7ec3e19b4a59718baf7e
ScottAgirs <scott@ijs.to>
2022-03-19 14:22:42 +0200

    refactor(User):  add onboarding checkbox
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/user/User.ts
+++ api/user/User.ts
@@ -1,24 +1,23 @@
-
-import { list } from '@keystone-6/core';
+import { list } from "@keystone-6/core";
 import {
   text,
   relationship,
   timestamp,
   checkbox,
-} from '@keystone-6/core/fields';
-import { afterCreateUser } from './afterCreateUser';
+} from "@keystone-6/core/fields";
+import { afterCreateUser } from "./afterCreateUser";
 
 export const User = list({
   hooks: {
     afterOperation: async ({ context, item, operation }) => {
-      if (operation === 'create') {
-        afterCreateUser({ context, item })
+      if (operation === "create") {
+        afterCreateUser({ context, item });
       }
     },
   },
   fields: {
     subjectId: text({
-      isIndexed: 'unique' 
+      isIndexed: "unique",
     }),
     // Address start
     country: text(),
@@ -30,35 +29,37 @@ export const User = list({
     thoroughfare: text(),
     premise: text(),
     // Address end
-    contracts: relationship({ ref: 'Contract.signedBy', many: true }),
+    contracts: relationship({ ref: "Contract.signedBy", many: true }),
     dateOfBirth: timestamp(),
-    doctor: relationship({ ref: 'Doctor.user' }),
+    doctor: relationship({ ref: "Doctor.user" }),
     email: text({
       validation: { isRequired: true },
-      isIndexed: 'unique',
+      isIndexed: "unique",
       isFilterable: true,
     }),
     firstName: text(),
+    isDummy: checkbox(),
+    isOnboarded: checkbox(),
+    isOnboardedDoctor: checkbox(),
     lastName: text(),
     middleName: text(),
-    isDummy: checkbox(),
     // password: password({ validation: { isRequired: true } }),
     // Phone numbers start
     cellPhoneNumberString: text(),
     homePhoneNumberString: text(),
     // Phone numbers end
-    prompts: relationship({ ref: 'Prompt.user', many: true }),
-    patient: relationship({ ref: 'Patient.user' }),
+    prompts: relationship({ ref: "Prompt.user", many: true }),
+    patient: relationship({ ref: "Patient.user" }),
     photoSrc: text(),
     sex: text(),
-    stepperProgs: relationship({ ref: 'StepperProg.user', many: true }),
-    stepperStepProgs: relationship({ ref: 'StepperStepProg.user', many: true }),
-    username: text({ isIndexed: 'unique', validation: { isRequired: true} }),
+    stepperProgs: relationship({ ref: "StepperProg.user", many: true }),
+    stepperStepProgs: relationship({ ref: "StepperStepProg.user", many: true }),
+    username: text({ isIndexed: "unique", validation: { isRequired: true } }),
   },
   ui: {
-    labelField: 'email',
+    labelField: "email",
     listView: {
-      initialColumns: ['firstName', 'lastName', 'email', "id"],
+      initialColumns: ["firstName", "lastName", "email", "id"],
     },
   },
-})
+});
