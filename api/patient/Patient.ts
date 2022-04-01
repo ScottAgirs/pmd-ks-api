8db9b22d49840fc770d3c2ba5ba665935085696e
ScottAgirs <scott@ijs.to>
2022-04-01 17:21:54 +0200

    chore(Patient): lint
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/patient/Patient.ts
+++ api/patient/Patient.ts
@@ -4,19 +4,28 @@ const { list } = require("@keystone-6/core");
 
 export const Patient = list({
   fields: {
-    appointments: relationship({ ref: 'Appointment.patient', many: true }),
-    appointmentVitals: relationship({ ref: 'AppointmentVital.patient', many: true }),
-    appointmentNotes: relationship({ ref: 'AppointmentNote.patient', many: true }),
-    bookings: relationship({ ref: 'Booking.patient', many: true }),
-    forms: relationship({ ref: 'Form.patient', many: true }),
-    healthCards: relationship({ ref: 'HealthCard.patient', many: true }),
     isCompleteProfile: checkbox(),
     name: text(),
-    medications: relationship({ ref: 'Medication.patient', many: true }),
-    prescriptions: relationship({ ref: 'Prescription.patient', many: true }),
-    savedDoctors: relationship({ ref: 'Doctor.savedByPatients', many: true }),
-    user: relationship({ ref: 'User.patient' }),
-    visitedDoctors: relationship({ ref: 'Doctor.patients', many: true }),
-    emergencyContacts: relationship({ ref: 'EmergencyContact.patient', many: true }),
+    appointments: relationship({ ref: "Appointment.patient", many: true }),
+    appointmentVitals: relationship({
+      ref: "AppointmentVital.patient",
+      many: true,
+    }),
+    appointmentNotes: relationship({
+      ref: "AppointmentNote.patient",
+      many: true,
+    }),
+    bookings: relationship({ ref: "Booking.patient", many: true }),
+    emergencyContacts: relationship({
+      ref: "EmergencyContact.patient",
+      many: true,
+    }),
+    forms: relationship({ ref: "Form.patient", many: true }),
+    healthCards: relationship({ ref: "HealthCard.patient", many: true }),
+    medications: relationship({ ref: "Medication.patient", many: true }),
+    prescriptions: relationship({ ref: "Prescription.patient", many: true }),
+    savedDoctors: relationship({ ref: "Doctor.savedByPatients", many: true }),
+    user: relationship({ ref: "User.patient" }),
+    visitedDoctors: relationship({ ref: "Doctor.patients", many: true }),
   },
-})
+});
