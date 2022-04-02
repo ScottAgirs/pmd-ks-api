e90b3c9959fd72454ae90dfbbec6e75dfef48c41
ScottAgirs <scott@ijs.to>
2022-04-02 09:28:37 +0200

    refactor(EmCon): rel not required
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/emergency-contact/EmergencyContact.ts
+++ api/emergency-contact/EmergencyContact.ts
@@ -5,11 +5,11 @@ const { list } = require("@keystone-6/core");
 export const EmergencyContact = list({
   fields: {
     email: text({ validation: { isRequired: true } }),
-    relation: text({ validation: { isRequired: true } }),
+    relation: text(),
     firstName: text({ validation: { isRequired: true } }),
     lastName: text({ validation: { isRequired: true } }),
     cellPhoneNumberString: text({ validation: { isRequired: true } }),
     homePhoneNumberString: text(),
-    patient: relationship({ ref: 'Patient.emergencyContacts' }),
+    patient: relationship({ ref: "Patient.emergencyContacts" }),
   },
-}) 
+});
