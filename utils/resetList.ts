4845a0d8c553257cbe25eb4c05d5391e51af9214
ScottAgirs <scott@ijs.to>
2022-04-02 10:46:34 +0200

    chore: Log steps on resetList
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- utils/resetList.ts
+++ utils/resetList.ts
@@ -1,15 +1,21 @@
 import { KeystoneContext } from "@keystone-6/core/types";
 
 export const resetList = async (listName: String, context: KeystoneContext) => {
+  console.log("About to fetch and reset");
   const allItems = await context.db[listName as string].findMany();
+  console.log("resetList :: allItems.length", allItems?.length);
   if (allItems.length > 0) {
-    const deleted = await context.db[listName as string].deleteMany({
-      where: allItems.map((i) => ({ id: i.id as string })),
-    });
-    console.log("resetList :: deleted", deleted);
-    return console.log(
-      `Deleted all (total of ${deleted.length}) items from ${listName} list.`
-    );
+    try {
+      const deleted = await context.db[listName as string].deleteMany({
+        where: allItems.map((i) => ({ id: i.id as string })),
+      });
+      console.log("resetList :: deleted", deleted);
+      return console.log(
+        `Deleted all (total of ${deleted.length}) items from ${listName} list.`
+      );
+    } catch (error) {
+      console.log("resetList :: error", error);
+    }
   } else {
     console.log(`resetList :: no ${listName} items to delete`);
     return null;
