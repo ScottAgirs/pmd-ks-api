dfdbe3540822c0f8cda87ac3878f7d51f1879e33
ScottAgirs <scott@ijs.to>
2022-05-03 01:24:14 +0300

    feat(Lists): ++ PoLicence
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ api/proof-of-license/model/ProofOfLicense.ts
@@ -0,0 +1,48 @@
+import "dotenv/config";
+
+import { checkbox, relationship, text } from "@keystone-6/core/fields";
+import { cloudinaryImage } from "@keystone-6/cloudinary";
+import { cloudinary, cloudinaryConfig } from "../../../lib/cloudinary";
+import { KeystoneContext } from "@keystone-6/core/types";
+
+const { list } = require("@keystone-6/core");
+
+export const ProofOfLicense = list({
+  ui: {
+    // isHidden: true,
+  },
+  hooks: {
+    afterOperation: async ({
+      context,
+      item,
+      operation,
+      originalItem,
+    }: {
+      context: KeystoneContext;
+      item: any;
+      operation: string;
+      originalItem: any;
+    }) => {
+      if (operation === "delete") {
+        cloudinary.uploader.destroy(
+          originalItem.src._meta.public_id,
+          {
+            invalidate: true,
+          },
+          (error, result) => {
+            console.log(result, error);
+          }
+        );
+      }
+    },
+  },
+  fields: {
+    src: cloudinaryImage({
+      cloudinary: { ...cloudinaryConfig, folder: "proof-of-license" },
+      label: "Source",
+    }),
+    altText: text(),
+    doctor: relationship({ ref: "Doctor.proofOfLicense" }),
+    isVerified: checkbox(),
+  },
+});
