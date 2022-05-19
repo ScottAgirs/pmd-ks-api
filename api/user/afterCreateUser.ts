e33544e2080ee375c094676ebe13c075ef6d6f7b
ScottAgirs <scott@ijs.to>
2022-05-19 23:22:23 +0300

    chore: lint
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/user/afterCreateUser.ts
+++ api/user/afterCreateUser.ts
@@ -1,46 +1,47 @@
-import { sendEmail } from "../../lib/email/sendEmail"
+import { BaseItem, KeystoneContext } from "@keystone-6/core/types";
+import { sendEmail } from "../../lib/email/sendEmail";
 
 // TODO: [TypeScript] Add context interface
-export const afterCreateUser = async ({ context, item }) => {
-  if (!item) throw new Error('Failed to create User item.')
-  
+export const afterCreateUser = async ({
+  context,
+  item,
+}: {
+  context: KeystoneContext;
+  item: BaseItem;
+}) => {
+  if (!item) throw new Error("Failed to create User item.");
+
   if (!item.isDummy) {
-    await context.query.StepperProg.createOne({ data: {
-      name: `${item.firstName}'s Stepper Progress`,
-      user: {
-        connect: {
-          id: item.id
-        }
+    await context.query.StepperProg.createOne({
+      data: {
+        name: `${item.firstName}'s Stepper Progress`,
+        user: {
+          connect: {
+            id: item.id,
+          },
+        },
       },
-    }})
-  
-  // await context.query.Prompt.createOne({ data: {
-  //   name: `${item.firstName}'s Patient Onboarding Prompt`,
-  //   promptType: "redirect",
-  //   promptValue: "/onboard/patient",
-  //   user: {
-  //     connect: {
-  //       id: item.id
-  //     }
-  //   },
-  // }})
-    await context.query.Patient.createOne({ data: {
-      name: `${item.firstName}'s Patient Profile`,
-      user: {
-        connect: {
-          id: item.id
-        }
+    });
+
+    await context.query.Patient.createOne({
+      data: {
+        name: `${item.firstName}'s Patient Profile`,
+        user: {
+          connect: {
+            id: item.id,
+          },
+        },
       },
-    }})
+    });
 
     sendEmail({
-      from:{
-        email:"test@pocketmd.ca",
-        name:"PocketMD Tester"
+      from: {
+        email: "test@pocketmd.ca",
+        name: "PocketMD Tester",
       },
-      to: item.email,
-      subject:"Welcome to PocketMD",
-      text:"This is your registration confirmation with PocketMD.",
-    })
+      to: item.email as string,
+      subject: "Welcome to PocketMD",
+      text: "This is your registration confirmation with PocketMD.",
+    });
   }
-}
+};
