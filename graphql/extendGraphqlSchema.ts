6997a20cdf971022ae01b6d46a9368413db568b6
ScottAgirs <scott@ijs.to>
2022-05-30 21:18:14 +0300

    feat(GQL): extendSchema ++ addPatientToDoctor
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- graphql/extendGraphqlSchema.ts
+++ graphql/extendGraphqlSchema.ts
@@ -1,19 +1,20 @@
 import { graphQLSchemaExtension } from "@keystone-6/core";
 
+import { addPatientToDoctor } from "../api/patient/resolvers/addPatientToDoctor";
 import { completeMyStepProg } from "../api/stepper-step-prog/resolvers/completeMyStepProg";
 import { createCalendarEventBooking } from "../api/booking/resolvers/createCalendarEventBooking";
 
 import { typeDefs } from "./typeDefs";
-
 
 export const extendGraphqlSchema = graphQLSchemaExtension({
   typeDefs,
   resolvers: {
     Mutation: {
       // Bookings
+      addPatientToDoctor,
       createCalendarEventBooking,
       // Stepper
       completeMyStepProg,
-    }
-  }
-})
+    },
+  },
+});
--- graphql/typeDefs.ts
+++ graphql/typeDefs.ts
@@ -1,10 +1,20 @@
 const graphql = String.raw;
 
-export const typeDefs =  graphql`
+export const typeDefs = graphql`
   type Mutation {
     # Bookings
-    createCalendarEventBooking(eventId: ID!, reason: String, startsAt: DateTime): StepperStepProg
+    createCalendarEventBooking(
+      eventId: ID!
+      reason: String
+      startsAt: DateTime
+    ): StepperStepProg
+    addPatientToDoctor(
+      firstName: String!
+      lastName: String!
+      email: String!
+      cellPhoneNumberString: String!
+    ): Patient
     # Stepper
     completeMyStepProg(stepId: ID!): StepperStepProg
   }
-`
+`;
