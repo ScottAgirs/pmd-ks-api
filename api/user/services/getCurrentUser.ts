90367d909c207fa71facf82ef6053d27db2c2c3b
ScottAgirs <scott@ijs.to>
2022-06-01 02:59:32 +0300

    chore(User): [WIP] getCurrentUser ++ types
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/user/services/getCurrentUser.ts
+++ api/user/services/getCurrentUser.ts
@@ -1,27 +1,35 @@
 import { KeystoneContext } from "@keystone-6/core/types";
 
+interface EnrichedCurrentUser {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  doctorId?: string;
+  patientId: string;
+  userId: string;
+}
+
 export const getCurrentUser = async (context: KeystoneContext) => {
   const userId = context.session?.data?.id;
   if (!userId) return null;
 
   try {
-    const currentUser = await context.query.User.findOne({ 
+    const currentUser = await context.query.User.findOne({
       where: { id: userId },
-      query: 'id username patient { id user { id } }',
+      query: "id username patient { id user { id } }",
     });
 
-  const enrichedCurrentUser = {
-    ...currentUser,
-    doctorId: currentUser.doctor?.id,
-    patientId: currentUser.patient?.id,
-    userId: currentUser.id,
-  }
+    const enrichedCurrentUser = {
+      ...currentUser,
+      doctorId: currentUser.doctor?.id,
+      patientId: currentUser.patient?.id,
+      userId: currentUser.id,
+    } as EnrichedCurrentUser;
 
-  return enrichedCurrentUser;
-    
+    return enrichedCurrentUser;
   } catch (error) {
     console.log(error);
     throw error;
   }
-    
-}
+};
