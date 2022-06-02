652f6f4ad70edcbf847237f17b450bcae775f837
ScottAgirs <scott@ijs.to>
2022-06-02 14:58:22 +0300

    chore: Cleanup
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- api/health-card/HealthCard.ts
+++ api/health-card/HealthCard.ts
@@ -1,17 +1,22 @@
-import { checkbox, integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+import {
+  checkbox,
+  relationship,
+  text,
+  timestamp,
+} from "@keystone-6/core/fields";
 
 const { list } = require("@keystone-6/core");
 
 export const HealthCard = list({
   fields: {
-    country: relationship({ ref: 'Country.healthCards' }),
     dateOfBirth: timestamp({ validation: { isRequired: true } }),
     expiryDate: timestamp({ validation: { isRequired: true } }),
     insurancePolicyNumber: text({ validation: { isRequired: true } }),
     insuranceProvider: text(),
     isVerified: checkbox(),
     nameOnCard: text({ validation: { isRequired: true } }),
-    patient: relationship({ ref: 'Patient.healthCards' }),
     versionCode: text({ validation: { isRequired: true } }),
+    country: relationship({ ref: "Country.healthCards" }),
+    patient: relationship({ ref: "Patient.healthCards" }),
   },
-}) 
+});
