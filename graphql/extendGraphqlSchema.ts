d2150103a9f623b1465b7fe9993fbfa131916596
ScottAgirs <scott@ijs.to>
2022-06-08 17:16:54 +0300

    feat(Schema): extend inviteUserByDoctor
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- graphql/extendGraphqlSchema.ts
+++ graphql/extendGraphqlSchema.ts
@@ -1,5 +1,6 @@
 import { graphQLSchemaExtension } from "@keystone-6/core";
 
+import { inviteUserByDoctor } from "../api/patient/resolvers/inviteUserByDoctor";
 import { addPatientToDoctor } from "../api/patient/resolvers/addPatientToDoctor";
 import { completeMyStepProg } from "../api/stepper-step-prog/resolvers/completeMyStepProg";
 import { createCalendarEventBooking } from "../api/booking/resolvers/createCalendarEventBooking";
@@ -12,6 +13,7 @@ export const extendGraphqlSchema = graphQLSchemaExtension({
     Mutation: {
       // Bookings
       addPatientToDoctor,
+      inviteUserByDoctor,
       createCalendarEventBooking,
       // Stepper
       completeMyStepProg,
