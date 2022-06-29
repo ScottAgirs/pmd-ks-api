ebbb2e5654c8d2df4eb390dbc9307abaf1363919
ScottAgirs <scott@ijs.to>
2022-06-29 18:34:40 -0400

    feat(GQL): extend Scheme acceptInvite

--- graphql/extendGraphqlSchema.ts
+++ graphql/extendGraphqlSchema.ts
@@ -1,6 +1,7 @@
 import { graphQLSchemaExtension } from "@keystone-6/core";
 
 import { inviteUserByDoctor } from "../api/patient/resolvers/inviteUserByDoctor";
+import { acceptInvite } from "../api/user-invite/services/accept-invite";
 import { addPatientToDoctor } from "../api/patient/resolvers/addPatientToDoctor";
 import { completeMyStepProg } from "../api/stepper-step-prog/resolvers/completeMyStepProg";
 import { createCalendarEventBooking } from "../api/booking/resolvers/createCalendarEventBooking";
@@ -17,6 +18,8 @@ export const extendGraphqlSchema = graphQLSchemaExtension({
       createCalendarEventBooking,
       // Stepper
       completeMyStepProg,
+      // User Invites
+      acceptInvite,
     },
   },
 });
