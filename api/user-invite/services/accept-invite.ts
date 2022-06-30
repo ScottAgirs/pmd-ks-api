d57161a63c50889f973069a1eee1700ebb54af79
ScottAgirs <scott@ijs.to>
2022-06-30 16:59:27 -0400

    refactor(Invites): cleanups

--- api/user-invite/services/accept-invite.ts
+++ api/user-invite/services/accept-invite.ts
@@ -6,47 +6,69 @@ export const acceptInvite = async (
   context: KeystoneContext
 ): Promise<any> => {
   const currentUser = context.session?.data;
-  const patientDB = context.db.Patient;
-  const doctorDB = context.db.Doctor;
+  const userId = currentUser.id;
+  const patientId = currentUser.patient.id as string;
+
   const inviteDB = context.db.UserInvite;
-  const invite = await inviteDB.findOne({
+  const invite = await context.query.UserInvite.findOne({
     where: { id: inviteId },
+    query: "id healthCard { id } invitedByUser { id } firstName lastName dateOfBirth middleName cellPhoneNumberString sex",
   });
 
-  const patient = await patientDB.findOne({
-    where: { id: currentUser.patient.id },
-  });
-  console.log("🚀 ~ line 22 ~ patient", patient)
-  const doctor = await doctorDB.findOne({
-    where: {
-      id: invite.invitedByUserId as string
-    },
-  });
+  const invitingUserId = invite.invitedByUser.id as string;
+  const healthCardId = invite.healthCard.id as string;
+
+  let doctorId;
+  try {
+    const doctorUser = await context.query.User.findOne({
+      where: { id: invitingUserId },
+      query: "id doctor { id }",
+    });
+
+    doctorId = doctorUser.doctor.id;
+  } catch (error) {
+    console.log("accept-invite.ts doctorDB.findOne ~ error", error)
+  }
+
+  if (!doctorId) throw new Error("Doctor not found");
     
   try {
-    const patientInCare = await context.db.Patient.updateOne({
-      where: { id: patient.id as string },
+    const updatedInvitedPatient = await context.db.Patient.updateOne({
+      where: { id: patientId },
       data: {
-        user: {
-          invite: { 
-            connect: { id: inviteId }
-          }
-        },
+        healthCards: { connect: [{ id: healthCardId }] },
         caredByDoctors: {
-          connect: [{ id: doctor.id }],
-        },
-      },
-    });
-    await context.db.Doctor.updateOne({
-      where: { id: doctor.id as string },
-    data: {
-      caringForPatients: {
-          connect: [{ id: patientInCare.id }],
+          connect: [{ id: doctorId }],
         },
       },
     });
 
-    return patientInCare;
+    await context.db.User.updateOne({
+      where: { id: userId },
+      data: {
+        firstName: invite.firstName,
+        lastName: invite.lastName,
+        dateOfBirth: invite.dateOfBirth,
+        middleName: invite.middleName,
+        cellPhoneNumberString: invite.cellPhoneNumberString,
+        sex: invite.sex,
+        signUpInvite: { 
+          connect: { id: inviteId }
+        }
+      },
+    });
+
+    await context.db.Doctor.updateOne({
+      where: { id: doctorId },
+      data: {
+        caringForPatients: {
+          connect: [{ id: patientId }],
+        },
+      },
+    });
+
+
+    return updatedInvitedPatient;
   } catch (error) {
     console.log("GOT A error", error);
   }
