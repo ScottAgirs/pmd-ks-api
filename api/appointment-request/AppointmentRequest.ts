b201697ef40fbe629504c578448531aba3e6df8d
ScottAgirs <scott@ijs.to>
2022-08-05 02:52:07 -0400

    feat(Lists): ++ AppointmentRequest

---
+++ api/appointment-request/AppointmentRequest.ts
@@ -0,0 +1,32 @@
+import { integer, relationship, text, timestamp } from "@keystone-6/core/fields";
+import { beforeCreateAppointmentRequest } from "./hooks/beforeCreateAppointmentRequest";
+
+const { list } = require("@keystone-6/core");
+
+export const AppointmentRequest = list({
+  fields: {
+    createdAt: timestamp({
+      defaultValue: {
+        kind: "now"
+      }
+    }),
+    reason: text(),
+    doctor: relationship({ ref: 'Doctor.appointmentRequests' }),
+    eventType: relationship({ ref: "CalendarEventType.appointmentRequests" }),
+    patient: relationship({ ref: 'Patient.appointmentRequests' }),
+  },
+  hooks: {
+    // @ts-ignore
+    resolveInput: async ({ context, inputData, operation, resolvedData }) => {
+      if (operation === "update") {
+        return resolvedData;
+      }
+
+      if (operation === "create") {
+        await beforeCreateAppointmentRequest({ context, inputData, resolvedData });
+
+        return resolvedData;
+      }
+    },
+  },
+}) 
