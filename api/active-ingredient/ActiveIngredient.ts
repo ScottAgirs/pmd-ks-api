a266a0204550001e30c446a76835bdb8d794a358
Scott Agirs <scott@ijs.to>
2022-08-15 22:04:11 -0400

    refactor(ActIng): field types

--- api/active-ingredient/ActiveIngredient.ts
+++ api/active-ingredient/ActiveIngredient.ts
@@ -1,17 +1,14 @@
-import { float, integer, relationship, text } from '@keystone-6/core/fields';
+import { relationship, text } from '@keystone-6/core/fields';
 
 import { list } from '@keystone-6/core';
 
 export const ActiveIngredient = list({
   fields: {
-    drugCode: integer({ isIndexed: true }),
+    drugCode: text({ isIndexed: true }),
     ingredientName: text({ isIndexed: true }),
-    strengthCount: float(),
     strengthUnit: text(),
+    strengthValue: text(),
     // eslint-disable-next-line sort-keys
-    medication: relationship({
-      many: true,
-      ref: 'Medication.activeIngredients',
-    }),
+    medication: relationship({ ref: 'Medication.activeIngredients' }),
   },
 });
