092e70845be7e52a32709d912d4fc4c61a3cad67
Scott Agirs <scott@ijs.to>
2022-08-15 22:04:02 -0400

    refactor(Med): field types

--- api/medication/Medication.ts
+++ api/medication/Medication.ts
@@ -1,13 +1,12 @@
-import { integer, relationship, text } from '@keystone-6/core/fields';
+import { relationship, text } from '@keystone-6/core/fields';
 
 import { list } from '@keystone-6/core';
 
 export const Medication = list({
   fields: {
     brandName: text({ isIndexed: true }),
-    companyName: text({ isIndexed: true }),
-    drugCode: integer({ isIndexed: true }),
-    drugIdentificationNumber: integer({ isIndexed: 'unique' }),
+    drugCode: text({ isIndexed: 'unique' }),
+    drugIdentificationNumber: text({ isIndexed: 'unique' }),
     // eslint-disable-next-line sort-keys
     activeIngredients: relationship({
       many: true,
