eb832fe5c3bd9566c6f31d5ab7da95b8dc716d62
Scott Agirs <scott@ijs.to>
2022-08-17 23:25:10 -0400

    refactor(Billing): [WIP] BillingItem

--- api/appointment-billing-item/AppointmentBillingItem.ts
+++ api/appointment-billing-item/AppointmentBillingItem.ts
@@ -1,13 +1,14 @@
-import { relationship, text, timestamp } from '@keystone-6/core/fields';
+import { integer, relationship, text } from '@keystone-6/core/fields';
 
 import { list } from '@keystone-6/core';
 
 export const AppointmentBillingItem = list({
   fields: {
     diagnosisCode: text(),
-    feeCode: text(),
-    units: text(),
+    premiumCode: text(),
+    units: integer(),
     // eslint-disable-next-line sort-keys
     billing: relationship({ ref: 'AppointmentBilling.billingItems' }),
+    claim: relationship({ ref: 'BillingClaim.appointmentBillingItems' }),
   },
 });
