2d72eb829759d7efd148a20a5e1d34716b3c3aab
Scott Agirs <scott@ijs.to>
2022-08-24 18:01:01 -0400

    refactor(Prescription): add createdAt to item

--- api/prescription-item/PrescriptionItem.ts
+++ api/prescription-item/PrescriptionItem.ts
@@ -1,9 +1,14 @@
-import { relationship, text } from '@keystone-6/core/fields';
+import { relationship, text, timestamp } from '@keystone-6/core/fields';
 
 import { list } from '@keystone-6/core';
 
 export const PrescriptionItem = list({
   fields: {
+    createdAt: timestamp({
+      defaultValue: {
+        kind: 'now',
+      },
+    }),
     instructions: text(),
     luCode: text(),
     refills: text(),
