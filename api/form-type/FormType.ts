10517dc78f0a1159493d1d5f5f7d066cad3a8e24
Scott Agirs <ScottAgirs@users.noreply.github.com>
2022-09-06 13:43:34 -0700

    feat(FormType): ++ isActive, make unique

--- api/form-type/FormType.ts
+++ api/form-type/FormType.ts
@@ -1,11 +1,12 @@
-import { relationship, text } from '@keystone-6/core/fields';
+import { checkbox, relationship, text } from '@keystone-6/core/fields';
 
 import { list } from '@keystone-6/core';
 
 export const FormType = list({
   fields: {
+    isActive: checkbox(),
     name: text({ validation: { isRequired: true } }),
-    slug: text({ validation: { isRequired: true } }),
+    slug: text({ isIndexed: 'unique', validation: { isRequired: true } }),
     // eslint-disable-next-line sort-keys
     forms: relationship({ many: true, ref: 'Form.formType' }),
   },
